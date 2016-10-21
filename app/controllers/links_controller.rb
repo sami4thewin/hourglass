7f313c111f47cd2f5faf651bac9cf21e2b6a5fe3
sami4thewin <samiomarmohammed@gmail.com>
2016-10-21 02:23:34 -0400

    Everything works, now just front end.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,18 +6,21 @@ class LinksController < ApplicationController
 
   def create
     # find or create
-    link = Link.find_or_create_by(link_params)
-    # link = Link.new
-    # link.title = params[:link][:title]
-    # link.description = params[:link][:description]
-    # link.link = params[:link][:link]
-    # link.save
-    # link.user_links.build(expiration_params.merge({user_id: current_user.id}))
-    u_link = UserLink.new(user_id: current_user.id, link_id: link.id, expiration: params[:link][:user_link][:expiration].to_i)
-    if u_link.save
-      redirect_to link_path(link)
+    if link = Link.find_or_create_by(link_params)
+      # link = Link.new
+      # link.title = params[:link][:title]
+      # link.description = params[:link][:description]
+      # link.link = params[:link][:link]
+      # link.save
+      # link.user_links.build(expiration_params.merge({user_id: current_user.id}))
+      u_link = UserLink.new(user_id: current_user.id, link_id: link.id, expiration: params[:link][:user_link][:expiration].to_i)
+      if u_link.save
+        redirect_to link_path(link)
+      else
+        @link = Link.new
+        render :new
+      end
     else
-      @link = Link.new
       render :new
     end
   end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,15 +6,18 @@ class VideosController < ApplicationController
 
   def create
     # binding.pry
-    video = Video.find_or_create_by(video_params)
-    # video.description = params[:video][:description]
-    # video.link = params[:video][:link]
-    # video.save
-    u_vid = UserVideo.new(user_id: current_user.id, video_id: video.id, expiration: params[:video][:user_video][:expiration].to_i)
-    if u_vid.save
-      redirect_to video_path(video)
+    if video = Video.find_or_create_by(video_params)
+      # video.description = params[:video][:description]
+      # video.link = params[:video][:link]
+      # video.save
+      u_vid = UserVideo.new(user_id: current_user.id, video_id: video.id, expiration: params[:video][:user_video][:expiration].to_i)
+      if u_vid.save
+        redirect_to video_path(video)
+      else
+        @video = Video.new
+        render :new
+      end
     else
-      @video = Video.new
       render :new
     end
   end
