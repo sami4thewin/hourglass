1c283944a71db628fddca1f6a11fb12409863596
sami4thewin <samiomarmohammed@gmail.com>
2016-10-20 21:44:48 -0400

    Fixed top videos and links and delete

--- app/controllers/user_links_controller.rb
+++ app/controllers/user_links_controller.rb
@@ -1,2 +1,12 @@
 class UserLinksController < ApplicationController
+
+  def destroy
+    # binding.pry
+    link = UserLink.find_by(id: params[:id])
+    link.user.completed += 1
+    link.user.save
+    link.destroy
+    redirect_to '/'
+  end
+
 end
--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -1,2 +1,12 @@
 class UserVideosController < ApplicationController
+
+  def destroy
+    # binding.pry
+    vid = UserVideo.find_by(id: params[:id])
+    vid.user.completed += 1
+    vid.user.save
+    vid.destroy
+    redirect_to '/'
+  end
+
 end
