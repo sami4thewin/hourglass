46316590c9212d21ba7079e49ea372fac5b7e573
sami4thewin <samiomarmohammed@gmail.com>
2016-10-24 12:57:05 -0400

    Final

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -12,9 +12,4 @@ class StaticController < ApplicationController
   end
 end
 
-# 
-# <% if Link.top.user_links.count == 0 %>
-# <h3>There is no current top link, do your part and add one below!</h3>
-# <% else %>
-# <h3>The current top link is <%= link_to Link.top.title, link_path(Link.top) %>, it's been shared <%= Link.top.user_links.count %> times.</h3>
-# <% end %>
+#
